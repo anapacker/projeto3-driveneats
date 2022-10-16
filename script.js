509cab1f2e00a37d003e719e697c844104c388c3
Ana Packer <anaamaria.packer@gmail.com>
2022-10-16 14:59:26 -0300

    botao selecionar um produto

--- script.js
+++ script.js
@@ -1,10 +1,82 @@
-function selecionarProduto(pratoSelecionado) {
-    const pratoSelecionadoArteriormente = document.querySelector('.caixa-produto .selecionado');
-    if (pratoSelecionadoArteriormente !== null) {
-        pratoSelecionadoArteriormente.classList.remove('selecionado');
+function selecionarPrato(pratoSelecionado) {
+    const pratoSelecionadoAnteriormente = document.querySelector('.caixa-prato .selecionado');
+    if (pratoSelecionadoAnteriormente !== null) {
+        pratoSelecionadoAnteriormente.classList.remove('selecionado');
     }
-    pratoSelecionado.classList.toggle('selecionado');
-    const valorPratoSelecionado = document.querySelector('.caixa-produto .selecionado .valor');
-    console.log(valorPratoSelecionado.innerHTML);
+    const existePratoSelecionado = document.querySelector(".box-opcoes .selecionado");
+    if (existePratoSelecionado) {
+        existePratoSelecionado.classList.remove("selecionado");
+    }
+
+    pratoSelecionado.classList.add('selecionado');
 }
 
+function selecionarSuco(sucoSelecionado) {
+    const sucoSelecionadoAnteriormente = document.querySelector('.caixa-suco .selecionado');
+    if (sucoSelecionadoAnteriormente !== null) {
+        sucoSelecionadoAnteriormente.classList.remove('selecionado');
+    }
+
+    const existeSucoSelecionado = document.querySelector(".box-opcoes .selecionado");
+    if (existeSucoSelecionado) {
+        existeSucoSelecionado.classList.remove("selecionado");
+    }
+
+
+
+    sucoSelecionado.classList.add('selecionado');
+}
+
+function selecionarSobremesa(sobremesaSelecionado) {
+    const sobremesaSelecionadoAnteriormente = document.querySelector('.caixa-pudim .selecionado');
+    if (sobremesaSelecionadoAnteriormente !== null) {
+        sobremesaSelecionadoAnteriormente.classList.remove('selecionado');
+    }
+
+    const existeSobremesaSelecionado = document.querySelector(".box-opcoes .selecionado");
+    if (existeSobremesaSelecionado) {
+        existeSobremesaSelecionado.classList.remove("selecionado");
+    }
+
+    sobremesaSelecionado.classList.add('selecionado');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const pratoSelecionadoArteriormente = document.querySelector('.caixa-prato .selecionado');
+    // if (pratoSelecionadoArteriormente !== null) {
+    //     pratoSelecionadoArteriormente.classList.remove('selecionado');
+    // }
+    // pratoSelecionado.classList.toggle('selecionado');
+    // const valorPratoSelecionado = document.querySelector('.caixa-prato .selecionado .valor-sopa');
+    // console.log(valorPratoSelecionado);
+
+    // const valor = document.querySelector('.valor-sopa')}
+
+
+// function selecionarPrato() {
+//     const produtoSelecionadoArteriormente = document.querySelector(".caixa-prato ");
+//     produtoSelecionadoArteriormente.classList.toggle("selecionado")
+
+
+// }
